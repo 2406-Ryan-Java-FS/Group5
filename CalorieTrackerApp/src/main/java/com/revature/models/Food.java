7c11ea99cb0c4b1b3baa46edf5bb7d90b8d00648
oyunmintrio95 <oyunmintrio95@gmail.com>
2024-07-10 14:20:03 -0700

    updated models for food

---
+++ CalorieTrackerApp/src/main/java/com/revature/models/Food.java
@@ -0,0 +1,26 @@
+package com.revature.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "foods")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "f_id", updatable = false)
+    private int id;
+
+    @Column(name = "food_name", nullable = false)
+    private boolean foodName;
+
+    @Column(name = "calorie", nullable = false)
+    private int calorie;
+}
