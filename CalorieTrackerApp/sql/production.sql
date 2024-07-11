0109665375f169fc3b206bfe2a7a659a57dfa1af
oyunmintrio95 <oyunmintrio95@gmail.com>
2024-07-11 06:18:14 -0700

    adding sql script

---
+++ CalorieTrackerApp/sql/production.sql
@@ -0,0 +1,31 @@
+create table users(
+	u_id serial primary key,
+	username varchar(50) unique not null,
+	password varchar(50) not null CHECK (LENGTH(password) >=8)
+
+);
+
+create table profiles(
+	p_id serial primary key,
+	gender boolean not null,
+	height double precision not null,
+	weight double precision not null,
+	activity int not null,
+	calorie_goal int not null,
+	u_id int references users(u_id) on delete set null
+);
+
+create table foods(
+	f_id serial primary key,
+	food_name varchar(50) unique not null,
+	calorie int not null
+);
+
+create table calorie_track(
+	c_id serial primary key,
+	f_id int references foods(f_id) on delete set null,
+	u_id int references users(u_id) on delete set null,
+	serving double precision not null,
+	datetime timestamp not null default now()
+);
+
